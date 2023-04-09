51fdd44d121bb982b44d545883532cfe575bd550
Shankara Narayanan Vaidyanathan <vaidyanathan.sh@northeastern.edu>
2023-04-09 13:28:13 -0400

    initial code for the cpp version of bitstar

---
+++ cpp/unit_test2.cpp
@@ -0,0 +1,39 @@
+#include <bitstar.h>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    
+    Node* start = new Node(0.0, 0.0);
+    Node* goal = new Node(9.0, 9.0);
+    Eigen::MatrixXd map = Eigen::MatrixXd::Ones(10, 10);
+
+    Bit_star *tree = new Bit_star(*start, *goal, map);
+    
+    int iteration = 0;
+
+
+    while(true){
+
+        iteration++;
+
+
+
+
+
+
+    return 0;
+}
